465748904aaf237078b5597f221ccf48f0e21a5c
squeek <squeek502@hotmail.com>
2015-04-28 01:16:42 -0700

    Tiny correction

--- examples/lit_app/package.lua
+++ examples/lit_app/package.lua
@@ -1,7 +1,7 @@
 return {
   name = "heroku-luvit/lit-example",
   version = "1.0.0",
-  description = "Example luvit app for heroku",
+  description = "Example lit app for heroku",
   dependencies = {
     "luvit/luvit"
   },
